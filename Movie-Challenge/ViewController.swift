6a57f5f3180ccd9a69bcb935246e25bcc49983b2
Vitor Campos <vito.r.natal@hotmail.com>
2021-06-04 12:35:23 -0300

    Initial Commit

---
+++ Movie-Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie-Challenge
+//
+//  Created by Thiago Pontes Lima on 04/06/21.
+//  Copyright © 2021 Vitor Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
